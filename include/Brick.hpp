5d7f05a3f6e9e9d78929aa56c23eabb003aadd1b
Uripro7 <160290059+Uripro7@users.noreply.github.com>
2024-06-24 22:31:41 +0000

    version2.7

--- include/Brick.hpp
+++ include/Brick.hpp
@@ -5,6 +5,8 @@ class Brick {
 public:
     Brick();
     Brick(int x, int y);
+    static const int WIDTH = 6;
+    static const int HEIGHT = 4;
     void draw();
     bool isActive() const;
     void deactivate();
