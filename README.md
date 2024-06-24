53c62654f20589e88eea45b9fcc89bc01aa6ebdd
Uripro7 <160290059+Uripro7@users.noreply.github.com>
2024-06-24 22:16:55 +0000

    version2.5

--- README.md
+++ README.md
@@ -1,18 +1,36 @@
 # brickbreacker
-## Proyecto progra
+## English and Programming Project
+### A bit of the history of the game
 ### Brickbreacker is a retro game that we all know and have played at some time, created in 1976 and inspired by the game "pong", breakout was the bases for many later games. Well, it is based on three objects only, a ball, a paddle and bricks that you must break, and how will you do it? Simple, with the ball. 
-
-## Ball
+##
+### Ball
 ### The ball is everything in the game, it is the object that will make you progress in the game, with the ball you must break the bricks but it is not as simple as it seems, you must prevent the ball from falling, with what? With the paddle.
 ![alt text](bola.png)
-
-## Paddle
+##
+### Paddle
 ### The paddle is the object that you control in the game, it is your only way to not lose in the game, you have three lives only, if you lose one the game restarts and you will have to break the bricks again then everything will be down to your dexterity to avoid the ball to go away and lose your score.
 ![alt text](paleta.png)
- 
- ## Brick
+ ##
+ ### Brick
  ### The brick is what you must break with the ball and the paddle, to win you must break each and every one of the bricks, each brick will give you score.
  ![alt text](ladrillos.png)
-
-  ## Here is a video of the videogame
+##
+ ### Here is a video of the videogame
 <video controls src="Ball.cpp - proyecto-breakout [Codespaces_ glowing space spork] - Visual Studio Code y 4 páginas más - Personal_ Microsoft​ Edge 2024-06-20 00-19-45.mp4" title="Title"></video>
+
+##
+### UML diagram
+
+
+##
+### Folder hpp
+![alt text](hpp.png)
+
+##
+### Folder src
+![alt text](cpp.png)
+
+
+
+##
+### This program on github is executed using the "make run" command, using the SFML, nsources libraries, among other libraries that are used in C++
--- src/Game.cpp
+++ src/Game.cpp
@@ -9,12 +9,13 @@ Game::Game() : score(0), lives(3), ball(WIDTH / 2, HEIGHT - 3, 1, -1), paddle(WI
 void Game::initGame() {
     for (int i = 0; i < 10; ++i) {
         for (int j = 0; j < 5; ++j) {
-            bricks[i][j] = Brick(i * 8, j * 2);
+            bricks[i][j] = Brick(i * Brick::WIDTH, j * Brick::HEIGHT);
         }
     }
 }
 
 void Game::run() {
+    showMenu();
     while (lives > 0) {
         drawGame();
         processInput();
@@ -29,52 +30,83 @@ void Game::drawGame() {
     paddle.draw();
     for (int i = 0; i < 10; ++i) {
         for (int j = 0; j < 5; ++j) {
-            bricks[i][j].draw();
+            if (bricks[i][j].isActive()) {
+                bricks[i][j].draw();
+            }
         }
     }
     life.draw(lives);
+    mvprintw(HEIGHT + 1, 0, "Score: %d", score);
     screenManager.refreshScreen();
 }
 
 void Game::updateGame() {
     ball.move();
+
+    // Check collision with paddle
     if (ball.getY() == HEIGHT - 1 && ball.getX() >= paddle.getX() && ball.getX() < paddle.getX() + paddle.getLength()) {
         ball.changeDirectionY();
+        // Adjust ball's direction based on where it hits the paddle
+        int paddleCenter = paddle.getX() + paddle.getLength() / 2;
+        int ballPosition = ball.getX();
+        if (ballPosition < paddleCenter) {
+            ball.changeDirectionX();
+        } else if (ballPosition > paddleCenter) {
+            ball.changeDirectionX();
+        }
     }
+
+    // Check collision with bricks
     for (int i = 0; i < 10; ++i) {
         for (int j = 0; j < 5; ++j) {
-            if (bricks[i][j].isActive() && ball.getX() == bricks[i][j].getX() && ball.getY() == bricks[i][j].getY()) {
+            if (bricks[i][j].isActive() && ball.getX() >= bricks[i][j].getX() && ball.getX() < bricks[i][j].getX() + Brick::WIDTH &&
+                ball.getY() >= bricks[i][j].getY() && ball.getY() < bricks[i][j].getY() + Brick::HEIGHT) {
                 bricks[i][j].deactivate();
                 ball.changeDirectionY();
                 score += 10;
             }
         }
     }
+
+    // Check if ball hits top wall
+    if (ball.getY() <= 0) {
+        ball.changeDirectionY();
+    }
+
+    // Check if ball hits side walls
+    if (ball.getX() <= 0 || ball.getX() >= WIDTH - 1) {
+        ball.changeDirectionX();
+    }
+
+    // Check if ball misses paddle and loses a life
     if (ball.getY() >= HEIGHT) {
         --lives;
-        ball = Ball(WIDTH / 2, HEIGHT - 3, 1, -1);
-        paddle = Paddle(WIDTH / 2 - PADDLE_LENGTH / 2, HEIGHT - 1, PADDLE_LENGTH);
+        ball = Ball(WIDTH / 2, HEIGHT - 3, 1, -1); // Reset ball position
+        paddle = Paddle(WIDTH / 2 - PADDLE_LENGTH / 2, HEIGHT - 1, PADDLE_LENGTH); // Reset paddle position
     }
 }
 
 void Game::processInput() {
     int ch = getch();
-    if (ch == KEY_LEFT && paddle.getX() > 0) {
-        paddle.moveLeft();
-    } else if (ch == KEY_RIGHT && paddle.getX() < WIDTH - paddle.getLength()) {
-        paddle.moveRight();
+    switch (ch) {
+        case KEY_LEFT:
+            paddle.moveLeft();
+            break;
+        case KEY_RIGHT:
+            paddle.moveRight();
+            break;
+        case 'q':
+            lives = 0; // Quit game
+            break;
+        default:
+            break;
     }
 }
 
 void Game::showMenu() {
     screenManager.clearScreen();
-    mvprintw(0, 0, "================================");
-    mvprintw(1, 0, "  Do you think you are fast?");
-    mvprintw(2, 0, "          Prove it B)");
-    mvprintw(3, 0, "      Welcome to breakout");
-    mvprintw(4, 0, "================================");
-    mvprintw(6, 0, "Press any key to start...");
+    mvprintw(HEIGHT / 2 - 2, WIDTH / 2 - 10, "Press any key to start...");
     screenManager.refreshScreen();
-    getch();
+    getch(); // Wait for user input to start game
     screenManager.clearScreen();
 }
